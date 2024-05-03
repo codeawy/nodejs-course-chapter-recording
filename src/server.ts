732746d83718593dd91ecba295ba76ff2667dc4c
codeawy <codeawy@gmail.com>
2024-05-03 06:15:55 +0300

    Query Params

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { generateFakeProducts } from "./utils/fakeData";
+import { Product } from "./interfaces";
 
 const app = express();
 
@@ -9,9 +10,29 @@ app.get("/", (req, res) => {
 
 const fakeProductsData = generateFakeProducts();
 
-// ** Endpoints (PRODUCTS)
 app.get("/products", (req, res) => {
-  res.send(fakeProductsData);
+  // ** Filter By, keyof Product
+  const filterQuery = req.query.filter as string;
+
+  if (filterQuery) {
+    const propertiesToFilter = filterQuery.split(",");
+
+    let filteredProducts = [];
+
+    filteredProducts = fakeProductsData.map(product => {
+      const filteredProduct: any = {};
+      propertiesToFilter.forEach(property => {
+        if (product.hasOwnProperty(property as keyof Product)) {
+          filteredProduct[property] = product[property as keyof Product];
+        }
+      });
+      return { id: product.id, ...filteredProduct };
+    });
+
+    return res.send(filteredProducts);
+  }
+
+  return res.send(fakeProductsData);
 });
 app.get("/products/:id", (req: Request, res: Response) => {
   console.log(req.params);
@@ -20,9 +41,7 @@ app.get("/products/:id", (req: Request, res: Response) => {
     res.status(404).send({ message: "Invalid product ID" });
   }
 
-  const findProduct: { id: number; title: string; price: number } | undefined = fakeProductsData.find(
-    product => product.id === productId
-  );
+  const findProduct: Product | undefined = fakeProductsData.find(product => product.id === productId);
   if (findProduct) {
     res.send({ id: productId, title: findProduct.title, price: findProduct.price });
   } else {
--- src/utils/fakeData.ts
+++ src/utils/fakeData.ts
@@ -1,11 +1,13 @@
 import { faker } from "@faker-js/faker";
+import { Product } from "../interfaces";
 
-export const generateFakeProducts = () => {
+export const generateFakeProducts = (): Product[] => {
   return Array.from({ length: 25 }, (_, idx) => {
     return {
       id: idx + 1,
       title: faker.commerce.productName(),
       price: +faker.commerce.price({ min: 100, max: 200 }),
+      description: faker.commerce.productDescription(),
     };
   });
 };
