19b88dcf4e16f840f6b41dd98113fb5e8df94a14
codeawy <codeawy@gmail.com>
2024-05-16 11:05:10 +0300

    More views

--- server.ts
+++ server.ts
@@ -15,20 +15,28 @@ app.set("view engine", "pug");
 // * Static file
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
 const fakeProductsData = generateFakeProducts();
 
 const productService = new ProductService(fakeProductsData);
 const productController = new ProductController(productService);
 
-app.get("/products", (req, res) => productController.getProducts(req, res));
-app.get("/products/:id", (req, res) => productController.getProductById(req, res));
-app.post("/products", (req, res) => productController.createProduct(req, res));
-app.patch("/products/:id", (req, res) => productController.updateProduct(req, res));
-app.delete("/products/:id", (req, res) => productController.deleteProduct(req, res));
+// ** Products Route
+app.get("/products", (req, res) => {
+  res.render("products");
+});
+app.get("/api/products", (req, res) => productController.getProducts(req, res));
+app.get("/api/products/:id", (req, res) => productController.getProductById(req, res));
+app.post("/api/products", (req, res) => productController.createProduct(req, res));
+app.patch("/api/products/:id", (req, res) => productController.updateProduct(req, res));
+app.delete("/api/products/:id", (req, res) => productController.deleteProduct(req, res));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("*", (req, res) => {
+  res.render("notFound");
+});
 
 const PORT: number = 5000;
 app.listen(PORT, () => {
