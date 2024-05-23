f6620caf96b2ab905b7bdd434f6fd6c4ecab4359
codeawy <codeawy@gmail.com>
2024-05-23 21:22:30 +0300

    Error page

--- middlewares/Error.ts
+++ middlewares/Error.ts
@@ -3,14 +3,20 @@ import { NextFunction, Request, Response } from "express";
 export default class ErrorMiddleware {
   static handle(err: Error, req: Request, res: Response, next: NextFunction) {
     // * API & Views
-    console.log(process.env.NODE_ENV);
     if (req.originalUrl.startsWith("/api")) {
       res.status(500).json({
         error: "Internal Server Error",
         message: err.message,
         stack: process.env.NODE_ENV === "development" ? err.stack : null,
       });
+      return;
     }
+
+    res.status(500).render("error", {
+      pageTitle: "Error",
+      message: "Something went wrong. Please try again later.",
+      error: err.message,
+    });
 
     next();
   }
